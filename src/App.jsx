37a4bac4a9e395a369948eded0badf235bee9989
krishkansal <krishkansal7@gmail.com>
2022-11-30 19:07:25 +0530

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './components/Main.jsx';
+import './components/style.css';
+
+
+const App=()=>{
+    return(
+        <>
+            <Main/>
+        </>
+    )
+}
+
+export default App;
